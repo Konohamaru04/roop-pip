959485b9881deae75ad8a7f507cf4c44c675aea0
Amartya Deshmukh <dedsec.konohamaru@gmail.com>
2024-06-11 17:42:21 +0530

    Update Setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="roop-pip",
-    version="1.0.6-alpha",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
         "numpy==1.24.3",
